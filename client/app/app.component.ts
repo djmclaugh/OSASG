3b431bc2dcb5299565ea9525726c940c1918b47a
David McLaughlin <djmclaugh@gmail.com>
2017-01-21 15:05:08 -0500

    Angular 2 migration start

---
+++ client/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "osasg-client",
+  template: "<h1>OSASG Angular2 Client</h1>"
+})
+export class AppComponent {}
---
+++ client/public/javascript/generated/README.md
@@ -0,0 +1,1 @@
+This folder contains the generated javascript needed to run the client.
