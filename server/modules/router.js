6cbc8876c3a8cac9ac80afff154cedb0e309aca1
djmclaugh <djmclaugh@gmail.com>
2017-12-10 12:38:49 -0500

    Tweaks to make remote servers work

--- server/modules/router.js
+++ server/modules/router.js
@@ -9,7 +9,7 @@ var matchManager;
 var router = express.Router();
 
 router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://" + config.clientURL);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Methods: GET, POST");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
